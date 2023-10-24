2d2b4a0d49b01348ac195eaa194ddb5464397c26
yamatatsu <the.kinnikuman@gmail.com>
2023-10-25 08:49:33 +0900

    handler

--- src/handlers/apigateway/workspaces-gateways-list/index.ts
+++ src/handlers/apigateway/workspaces-gateways-list/index.ts
@@ -1,14 +1,4 @@
-import zod from "zod";
-import { Workspace, Gateway } from "../../../db/index.js";
 import wrap from "../common/wrapper.js";
-import { parsePathParameters } from "../common/event-util.js";
+import impl from "./impl.js";
 
-const paramsSchema = zod.object({
-  workspaceId: zod.string(),
-});
-
-export const handler = wrap((event) =>
-  parsePathParameters(event, paramsSchema)
-    .asyncAndThen((params) => Workspace.get(params.workspaceId))
-    .andThen((workspace) => Gateway.listByWorkspace(workspace)),
-);
+export const handler = wrap(impl);
